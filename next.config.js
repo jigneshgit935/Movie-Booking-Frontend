639faa6c8a255d009f51d09e1271ac8198962b58
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-25 14:09:02 +0530

    Home Slider created

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: { domains: ['in.bmscdm.com', 'assets-in.bmscdn.com'] },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
-import Image from 'next/image';
+'use client';
+
 import styles from './page.module.css';
+import HomeSlider from '@/Components/HomeSlider/HomeSlider';
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <HomeSlider />
+    </main>
+  );
 }
